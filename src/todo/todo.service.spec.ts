dbbd82b904b48b7d86427035d010e966c51b3a13
Ilyas Akin <ashnwor@gmail.com>
2021-07-11 20:23:07 +0300

    Fix test fail

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaModule } from '../_prisma/prisma.module';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
@@ -6,6 +7,7 @@ describe('TodoService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [TodoService],
     }).compile();
 
