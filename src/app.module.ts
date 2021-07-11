173287e85a7ba8cdf7e9ab49bc2fc011495b7c49
Ilyas Akin <ashnwor@gmail.com>
2021-07-11 11:59:48 +0300

    Create Prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo/todo.service';
 import { TodoModule } from './todo/todo.module';
+import { PrismaService } from './_prisma/prisma.service';
 
 @Module({
   imports: [TodoModule],
   controllers: [],
-  providers: [TodoService],
+  providers: [TodoService, PrismaService],
 })
 export class AppModule {}
